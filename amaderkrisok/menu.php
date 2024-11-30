295271437ee58a917d9aa5c6023b06bca1e0f143
Md Mehadi Hasan <129536488+mehadi214100@users.noreply.github.com>
2024-12-01 00:41:25 +0600

    Add files via upload

---
+++ amaderkrisok/menu.php
@@ -0,0 +1,15 @@
+<nav class="navbar">
+    <div class="logo">
+        <img class="log" src="image/logo.png" alt="">
+    </div>
+    <ul class="nav-links">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="#">Crop Information</a></li>
+        <li><a href="#">Disease Diagnosis</a></li>
+        <li><a href="#">Marketplace</a></li>
+        <li><a href="#">Expert Consultation</a></li>
+        <li><a href="labor_machinery.php">Labor & Machinery</a></li>
+        <li><a href="#">Loan Assistance</a></li>
+        <li><a class="Contact" href="logout.php">Logout</a></li>
+    </ul>
+</nav>
